608ab0c165ee375d3cd053c7fdf45014da0ea2f3
linhnguyen26 <linhnlnse140957@fpt.edu.vn>
2021-11-26 01:02:20 +0700

    adding full errror handling

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -18,10 +18,18 @@ axios.interceptors.response.use(
     return res;
   },
   (error: AxiosError) => {
-    const { data, status } = error.response!;
+    const { data, status, config } = error.response!;
 
     switch (status) {
       case 400:
+        if (typeof data === "string") {
+          toast.error(data);
+        }
+
+        if (config.method === "get" && data.errors.hasOwnProperty("id")) {
+          history.push("/not-found");
+        }
+
         if (data.errors) {
           console.log(data.errors);
           const modalStateErrors = [];
@@ -32,8 +40,6 @@ axios.interceptors.response.use(
             }
           }
           throw modalStateErrors.flat();
-        } else {
-          toast.error(data);
         }
         break;
       case 401:
